4a612218a8048896804e3724aa277e6e8b677e7f
Gabriel Ruiz <89103051+ISABEAST56@users.noreply.github.com>
2021-12-06 18:29:27 -0500

    Add files via upload

--- ClosestPoints/HashTable.java
+++ ClosestPoints/HashTable.java
@@ -3,23 +3,14 @@ public class HashTable
     private Node[][] table;
 	int numelements;
     private int size;
-	
-    /**
-    *	constructor for HashTable
-    *	@param int s: size given
-    **/
+
     public HashTable(int s)
     {
 	numelements = 0;
         size = s;
         table = new Node[size][size];
     }
-    
-    /**
-    *	insert method that takes two doubles and hashes into table
-    *	@param double x: double number given
-    *	@param double y: double nubmer given
-    **/
+
     public void insert(double x, double y)
     {
         int i = hash(x);
@@ -29,21 +20,11 @@ public class HashTable
 	    numelements++;
     }
 
-    /**
-    *	hash method that uses prime number modulo to hash.
-    *	@param coor: double number to be hashed
-    **/
     private int hash(double coor)
     {   
         return (int)(coor * 19219) % size;
     }
-	
-    /**
-    *	find method to check if given numbers are located in the hashtable and returns true or false.
-    *	@param double x: double value given
-    *	@param double y: double number given for second point
-    *	@param bool
-    **/
+
     public boolean find(double x, double y)
     {
         int i = hash(x);
@@ -59,28 +40,16 @@ public class HashTable
         return false;
     }
 
-    /**
-    *	getter method for size
-    *	@return size
-    **/
     public int getSize()
     {
         return size;
     }
     
-    /**
-    *	getTable method to grab current value at position [i][j]
-    *	@param int i, int j: given coors for table
-    *	@return table[i][j]: value at thise points.
-    **/
     public Node getTable(int i, int j)
     {
         return table[i][j];
     }
 
-    /**
-    *	print method
-    **/
     public void print()
     {
         int n = 0;
