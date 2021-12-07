131e08530b885a3f1d2967740cebf486acb22a58
Gabriel Ruiz <89103051+ISABEAST56@users.noreply.github.com>
2021-12-06 19:26:02 -0500

    Update NQueens.java

--- NQueens/NQueens.java
+++ NQueens/NQueens.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 
+/**
+*   NQueens program solves the NQueens by counting all possible solutions for n
+**/
 public class NQueens
 {
     private static int[][] board;
     private static int placements;
+    
 
+    /**
+    *   NQueens constructor that initializes board and placements
+    **/
     public NQueens(int n)
     {
         board = new int[n][n];
         placements = 0;
     }
     
-    //function to check if queen is safe to be placed
+    /**
+    *   function to check if queen is safe to be placed at location
+    *   @param int row: row number
+    *   @param int col: col number
+    *   @return bool
+    **/
     private boolean check_safe(int row, int col)
     {
     
-            //check column for other queen
+            //check current column for other queen
             for (int rowTrack = 0; rowTrack < row; rowTrack++)
             {
                 if (board[rowTrack][col] == 1)
@@ -42,8 +54,15 @@ public class NQueens
             return true;
     }
         
+    /**
+    *   check_row checks each row for valid placements using recursion.
+    *   Will backtrack to cut down time
+    *   @param int r: row number
+    **/
     public void check_row(int r)
     {
+        //if int r is equal or exceeds length of board then recursion is done
+        //as all possible legal placements have been reached for each recursion path.
         if (r >= board.length)
         {
             placements++;
@@ -52,12 +71,13 @@ public class NQueens
         else
         {
 
+            //for loop for as i traverses all rows of the board
             for (int i = 0; i < board.length; i++)
             {
 
                 if (check_safe(r, i) == true)
                 {
-                    board[r][i] = 1;
+                    board[r][i] = 1; //able to place queen at [r][i]
         
                     //recursion to place the other queens
                     check_row(r + 1);
@@ -69,7 +89,10 @@ public class NQueens
         }
     }
     
-
+    /**
+    *   main method to read in n given by user and call methods needed to caculate all possible placements for n
+    *   @param String[] args
+    **/
     public static void main(String[] args)
     {
         Scanner kb = new Scanner(System.in);
