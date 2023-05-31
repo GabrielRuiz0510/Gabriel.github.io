8ae84cb754594ca33fa8bd62eddc2a79efed4e06
Gabriel Ruiz <89103051+GabrielRuiz0510@users.noreply.github.com>
2023-05-31 13:39:52 -0400

    Update content_based_filtering_model.py

--- filter_model/content_based_filtering_model.py
+++ filter_model/content_based_filtering_model.py
@@ -15,7 +15,17 @@ import random
 
 class main:
     def __init__(self):
+        """
+        Initialize the main class.
+       
+        This constructor sets up the plotting style and configurations for matplotlib.
+        It also loads the data from a CSV file, performs sentence embedding using SentenceTransformer,
+        and performs PCA on the embedded data.
+        """
+        
+        # Set the styling and configuration for matplotlib
         plt.style.use('ggplot')
+        # Set the font family for text elements in the plots
         plt.rcParams['font.family'] = 'sans-serif' 
         plt.rcParams['font.serif'] = 'Ubuntu' 
         plt.rcParams['font.monospace'] = 'Ubuntu Mono' 
@@ -37,21 +47,25 @@ class main:
         'xkcd:scarlet']
         
         #%%
+        # Global variables
         global data
         global X
         global cos_sim_data
         
+        # Read data from a CSV file
         data = pd.read_csv('imdb_top_1000.csv')
         #X = np.array(data.Overview)
-        X = data["Overview"].tolist()
-        data = data[['Genre', 'Overview', 'Series_Title']]
+        X = data["Overview"].tolist() # Convert 'Overview' column to a list
+        data = data[['Genre', 'Overview', 'Series_Title']] # Select specific columns from the data
         data.head()
         
+        # Perform sentence embedding using SentenceTransformer
         text_data = X
         model = SentenceTransformer('all-MiniLM-L6-v2')
         embeddings = model.encode(text_data, show_progress_bar=True)
         
         #%%
+        # Perform PCA on the embedded data
         embed_data = embeddings
         X = np.array(embed_data)
         n_comp = 5
@@ -65,8 +79,22 @@ class main:
         sns.pairplot(pca_data)
     
     #%%
+        # Create a pairwise cosine similarity matrix    
         cos_sim_data = pd.DataFrame(cosine_similarity(X))
+    
     def give_recommendations(self, title = '',print_recommendation = False, print_recommendation_plots = False, print_genres = False):
+        """
+        Provide movie recommendations based on a given movie title.
+        
+        Args:
+            title (str): The movie title.
+            print_recommendation (bool): Whether to print the recommended movies.
+            print_recommendation_plots (bool): Whether to print the plots of the recommended movies.
+            print_genres (bool): Whether to print the genres of the recommended movies.
+        
+        Returns:
+            list: A list containing the movie list, plot list, and genre list.
+        """
         index = 0
         index = self.find_title(title)
         index_recomm = cos_sim_data.loc[index].sort_values(ascending = False).index.tolist()[1:6]
@@ -109,13 +137,32 @@ class main:
                 #print('The genre of the number %i recommended is this one:\n %s \n' %(k,plot_q))
                 k = k + 1
         return [movieList, plotList, genreList]
+    
     #%%
     def random_movie(self):
+        """
+        Provide recommendations for a random movie from the dataset.
+   
+        Args:
+            None
+   
+        Returns:
+            list: A list containing the movie list, plot list, and genre list of the recommended movie.
+        """
         num = random.randint(0, 999)
         return self.give_recommendations(data['Series_Title'].loc[num], True, True)
         
     #%%
     def find_title(self, title):
+        """
+        Find the index of a movie title in the dataset.
+    
+        Args:
+            title (str): The movie title to search for.
+    
+        Returns:
+            int: The index of the movie title in the dataset. If not found, returns the last index.
+        """
         indexNum = 0
         while indexNum < len(data.index):
             if title.casefold() == data['Series_Title'].loc[indexNum].casefold():
